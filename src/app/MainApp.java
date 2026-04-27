ad2570cfd092456eab4344987e6e12ba1184ea3f
Landon <landonp95@gmail.com>
2026-04-27 11:09:04 -0400

    starter code

---
+++ src/app/MainApp.java
@@ -0,0 +1,22 @@
+package app;
+
+import javax.swing.SwingUtilities;
+
+import ui.GameWindow;
+import model.GameModel;
+
+public class MainApp {
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(() -> {
+		new MainApp().run();
+		});
+		}
+	
+	// This runs only once at startup:
+	// MainApp creates and connects objects (no game logic here)
+	public void run() {
+		GameModel model = new GameModel();
+		GameWindow window = new GameWindow(model);
+		window.show();
+	}
+}
